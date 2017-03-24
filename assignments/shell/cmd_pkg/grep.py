c66b0167e310ea579b9772bf208b58e4a6c48834
revathich21 <revathi.kpf303@gmail.com>
2017-03-24 17:16:59 -0500

    Create grep.py

---
+++ assignments/shell/cmd_pkg/grep.py
@@ -0,0 +1,29 @@
+
+def grep(keyword,file,flag):
+        f=open(file,'r')
+	      list=[]
+	      if(flag==1):
+		
+		        for line in f:
+			            if keyword in line:
+                		  list.append(line)
+		    f.close()
+		    f1=open("default.txt",'w')
+		    for lines in list:
+			        f1.write(lines)
+	      else:
+            for line in f:
+			            if keyword in line:
+                		    print line
+def grepredirect(keyword,file1,file2):
+        f=open(file1,'r')
+        f1=open(file2,'w')
+        for line in f:
+            if keyword in line:
+			            f1.write(line)
+def grepapp(keyword,file1,file2):
+        f=open(file1,'r')
+        f1=open(file2,'a')
+        for line in f:
+            if keyword in line:
+			            f1.write(line)
