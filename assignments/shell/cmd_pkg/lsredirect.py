b353be87ceb1dac1f1e1569f86538fbc20838f54
revathich21 <revathi.kpf303@gmail.com>
2017-03-24 16:05:03 -0500

    Update lsredirect.py

--- assignments/shell/cmd_pkg/lsredirect.py
+++ assignments/shell/cmd_pkg/lsredirect.py
@@ -8,8 +8,8 @@ from os import stat
 """
 COMMAND NAME       :  ls(redirection)
 DESCRIPTION        :   List  all files from the current directory.
-	          -a      :    It shows all the hidden files.
-	          -l     :    Displays  long listing.
+	   -a      :    It shows all the hidden files.
+	    -l     :    Displays  long listing.
             -h     :    Human readable sizes
 
 """
@@ -23,15 +23,15 @@ def lsredirect(var,file,flag):
                 		    if i.startswith('.'):
                		 		      list.remove(i)
         		 		            list.sort()
-        				    for i in list:
+        				   	 for i in list:
              					  f.write(i)
     else :
-	      f=open(file,'w')
+	f=open(file,'w')
         flag=var
         if flag=='-l':
        		     list=os.listdir(os.getcwd())
-            	 list.sort()
-               for i in list:
+            	     list.sort()
+                     for i in list:
                 	if i.startswith('.'):
                     		list.remove(i)
          	     for i in range(len(list)):
@@ -43,14 +43,14 @@ def lsredirect(var,file,flag):
                 	str=p.__str__()+"\t"+name.__str__()+"\t"+ name.__str__() +"\t"+info.st_size.__str__()+"\t"+atime.__str__()+"\t"+list[i].__str__()
                 	f.write(str+"\n")
         elif flag=="-a":
-	            f=open(file,'w')
-	            flag=var	
+	      f=open(file,'w')
+	      flag=var	
               list=os.listdir(os.getcwd())
               list.sort()
               for i in list:
 		              f.write(i+"\t")
         elif flag=='-la':
-	            f=open(file,'w')
+	      f=open(file,'w')
               flag=var
               list=os.listdir(os.getcwd())
               list.sort()
@@ -69,16 +69,16 @@ def lsredirect(var,file,flag):
                 f.write(str+"\n")
         elif flag=="-ah":
 	
-	          f=open(file,'w')
-	          flag=var
+	    f=open(file,'w')
+	    flag=var
             list=os.listdir(os.getcwd())
             list.sort()
             for i in list:
                
 		                f.write(i+"\t")
         elif flag=='-lh':
-	          f=open(file,'w')
-	          flag=var
+	    f=open(file,'w')
+	    flag=var
 
             list=os.listdir(os.getcwd())
             list.sort()
@@ -94,8 +94,8 @@ def lsredirect(var,file,flag):
                 str=p.__str__()+" "+name.__str__()+" "+ name.__str__() +" "+s.__str__()+" "+atime.__str__()+" "+list[i].__str__()
                 f.write(str+"\t")
         elif flag=='-lah':
-	          f=open(file,'w')
-	          flag=var
+	    f=open(file,'w')
+	    flag=var
 
             list=os.listdir(os.getcwd())
             list.sort()
