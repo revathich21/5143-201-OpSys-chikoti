2826e4270f00540ed42a7a548849f11b510d49a9
revathich21 <revathi.kpf303@gmail.com>
2017-03-24 16:10:59 -0500

    Create mkdir.py

---
+++ assignments/shell/cmd_pkg/mkdir.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+"""
+COMMAND NAME        :    mkdir
+
+DESCRIPTION         :    To create a directory.
+
+PARAMETERS          :    Directory name
+
+"""
+def mkdir(cmd):
+        os.makedirs(cmd)
